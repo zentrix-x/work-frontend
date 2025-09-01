632e17dde8fb146b4e688e964a2d198bbc3ace13
aaa <aaa@email.com>
2025-09-01 11:47:23 +0200

    fixed/again

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,7 +6,7 @@ export default function Login() {
       <div className="flex flex-col gap-2 mt-4">
         <input type="email" placeholder="Email" className="p-2 border" />
         <input type="password" placeholder="Password" className="p-2 border" />
-        <a href="#" className="p-2 text-center text-white bg-blue-600 rounded">Login</a>
+        <a href="/" className="p-2 text-center text-white bg-blue-600 rounded">Login</a>
       </div>
     </div>
   );
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -6,7 +6,7 @@ export default function Register() {
       <div className="flex flex-col gap-2 mt-4">
         <input type="email" placeholder="Email" className="p-2 border" />
         <input type="password" placeholder="Password" className="p-2 border" />
-        <a href="#" className="p-2 text-center text-white bg-green-600 rounded">Register</a>
+        <a href="/login" className="p-2 text-center text-white bg-green-600 rounded">Register</a>
       </div>
     </div>
   );
